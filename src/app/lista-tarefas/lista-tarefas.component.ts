267670ec856cfc4b40ecab1ce86fc4395aad76f4
Nayanne Batista <nayannelopes.bd@gmail.com>
2023-05-05 11:39:35 -0300

    aula 4 finalizada

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -36,6 +36,7 @@ export class ListaTarefasComponent implements OnInit {
   campoBusca = '';
   tarefasFiltradas: Tarefa[] = [];
   tarefasSubscription: Subscription = new Subscription;
+  estadoBotao: string = 'unchecked'
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -133,6 +134,11 @@ export class ListaTarefasComponent implements OnInit {
   finalizarTarefa(tarefa: Tarefa) {
     this.id = tarefa.id
     this.service.atualizarStatusTarefa(tarefa)
+    if(tarefa.statusFinalizado == true) {
+      this.estadoBotao = 'checked'
+    } else {
+      this.estadoBotao = 'unchecked'
+    }
   }
 
   habilitarBotao(): string {
